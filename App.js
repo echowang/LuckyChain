94ef2817843eadeca5f55f3bacff1b57bc816ea5
王亭 <tingw@hwtc.com.cn>
2018-07-09 18:01:28 +0800

    tabbar

--- App.js
+++ App.js
@@ -7,43 +7,114 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StatusBar} from 'react-native'
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation'
+
+import color from './src/widget/color'
+import TabBarItem from './src/widget/TabBarItem'
+
+import HomeScene from './src/scene/HomeScene'
+import NearbyScene from './src/scene/NearbyScene'
+import OrderScene from './src/scene/OrderScene'
+import MineScene from './src/scene/MineScene'
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(Props){
+    super(Props)
+      StatusBar.setBarStyle('light-content')
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Navigator />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+
+const Tab = createBottomTabNavigator({
+    Home: {
+        screen: HomeScene,
+        navigationOptions: {
+            tabBarLabel: '团购',
+            tabBarIcon: ({focused, tintColor}) => (
+                <TabBarItem
+                    tintColor={tintColor}
+                    focused={focused}
+                    normalImage={require('./src/img/tabbar/tabbar_homepage.png')}
+                    selectedImage={require('./src/img/tabbar/tabbar_homepage_selected.png')}
+                />
+            )
+        },
+    },
+    Nearby: {
+        screen: NearbyScene,
+        navigationOptions: {
+            tabBarLabel: '附近',
+            tabBarIcon: ({focused, tintColor}) => (
+                <TabBarItem
+                    tintColor={tintColor}
+                    focused={focused}
+                    normalImage={require('./src/img/tabbar/tabbar_merchant.png')}
+                    selectedImage={require('./src/img/tabbar/tabbar_merchant_selected.png')}
+                />
+            )
+        },
+    },
+
+    Order: {
+        screen: OrderScene,
+        navigationOptions: {
+            tabBarLabel: '订单',
+            tabBarIcon: ({focused, tintColor}) => (
+                <TabBarItem
+                    tintColor={tintColor}
+                    focused={focused}
+                    normalImage={require('./src/img/tabbar/tabbar_order.png')}
+                    selectedImage={require('./src/img/tabbar/tabbar_order_selected.png')}
+                />
+            )
+        },
+    },
+
+    Mine: {
+        screen: MineScene,
+        navigationOptions: {
+            tabBarLabel: '我的',
+            tabBarIcon: ({focused, tintColor}) => (
+                <TabBarItem
+                    tintColor={tintColor}
+                    focused={focused}
+                    normalImage={require('./src/img/tabbar/tabbar_mine.png')}
+                    selectedImage={require('./src/img/tabbar/tabbar_mine_selected.png')}
+                />
+            )
+        },
+    },
+},{
+        lazy: true,
+        animationEnabled: false,
+        swipeEnabled: false,
+        tabBarOptions: {
+        activeTintColor: color.primary,
+            inactiveTintColor: color.gray,
+            style: {backgroundColor: '#ffffff'},
+    },
+})
+
+const Navigator = createStackNavigator(
+    {
+        Tab: {screen: Tab},
+    },
+    {
+      navigationOptions: {
+          title:"LuckyChain",
+          headerStyle: { backgroundColor: color.primary},
+          headerBackTitle: null,
+          headerTintColor: '#333333',
+          showIcon: true
+      },
+    })
---
+++ src/scene/OrderScene.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class OrderScene extends Component<Props>{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return (<View>
+            <Text style={{fontSize:20}}>OrderScene</Text>
+        </View>);
+    }
+}
